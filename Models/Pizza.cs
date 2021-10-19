d89ae6aa278647de6d1c19dcf8d78212e9a4bd86
hishirokaiyashi <hishirokaiyashi@gmail.com>
2021-10-19 13:55:12 +0700

    add something

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -5,5 +5,8 @@ namespace ContosoPizza.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsGlutenFree { get; set; }
+        //test-ABC
+        //chưa cài git bash hả
+        
     }
 }
