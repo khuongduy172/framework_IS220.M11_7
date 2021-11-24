afac7f6e6b2ddaddf3f0fe499e1ff59ff0f9bb78
TuanLeAnh1003 <19520331@gm.uit.edu.vn>
2021-11-24 21:00:10 +0700

    123

---
+++ Controllers/SmsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using Nexmo.Api.Messaging;
+using social_network.Hubs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace social_network.Controllers
+{
+    public class SmsController : Controller
+    {
+        public IHubContext<SmsHub> HubContext { get; set; }
+
+        public SmsController(IHubContext<SmsHub> hub)
+        {
+            HubContext = hub;
+        }
+        [HttpPost("webhooks/inbound-sms")]
+        public async Task<IActionResult> InboundSms()
+        {
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var json = await reader.ReadToEndAsync();
+                var inbound = JsonConvert.DeserializeObject<InboundSms>(json);
+                await HubContext.Clients.All.SendAsync("InboundSms", inbound.Msisdn, inbound.Text);
+            }
+            return NoContent();
+        }
+    }
+}
